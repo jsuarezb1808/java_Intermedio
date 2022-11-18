8eecd6045fdaa356011cbf0936c237f3509cbc92
jsuarezb1808 <96279058+jsuarezb1808@users.noreply.github.com>
2022-11-18 15:06:33 -0500

    class persona.java
    created class persona

---
+++ persona.java
@@ -0,0 +1,24 @@
+public class persona{
+{   
+    String nombre;
+    int edad;
+
+    public void persona(){
+        setNombre("human");
+        setEdad(0);
+    }
+    public void persona(string nombre, int edad){
+        setNombre(nombre);
+        setEdad(edad);
+    }
+
+    public void setNombre(string nombre){
+        this.nombre=nombre;
+    }
+
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+
+
+}
